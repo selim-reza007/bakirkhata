164e4213fc9e43c7b0da4c4cc7eb07e2b2990adb
Selim Reza <selim.reza.dia@gmail.com>
2024-11-06 10:50:13 +0600

    customer table altred and customer model class added

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    mobile = models.CharField(max_length=11)
+    balance = models.IntegerField()
+    picture = models.ImageField(default="fallback.jpg")
